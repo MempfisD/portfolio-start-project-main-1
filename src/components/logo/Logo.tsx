d1c70ece24eacf57deb821475ef077f9d02298a9
Dmitry Naumov <komrad.nda@yandex.ru>
2024-03-24 21:25:00 +0300

    add header structure

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Icon } from '../icon/Icon';
+
+export const Logo = () => {
+	return (
+		<a href=''>
+			<Icon width='62' height='62' viewBox='0 0 62 62' iconId={'logo'} />
+		</a>
+	);
+};
