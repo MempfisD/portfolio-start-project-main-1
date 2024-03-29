67065888c8e4fed813ae2f781a7578ba067276fa
Dmitry Naumov <komrad.nda@yandex.ru>
2024-03-29 20:30:40 +0300

    add ClobalStyled and Theme

--- src/lauoyt/header/Header.tsx
+++ src/lauoyt/header/Header.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Logo } from '../../components/logo/Logo';
-import { Menu } from '../../components/menu/Menu';
+import React from 'react'
+import styled from 'styled-components'
+import { Logo } from '../../components/logo/Logo'
+import { Menu } from '../../components/menu/Menu'
+import { Container } from '../../components/Container'
+import { FlexWrapper } from '../../components/FlexWrapper'
 
 export const Header = () => {
 	return (
 		<StyledHeader>
-			<Logo />
-			<Menu />
+			<Container>
+				<FlexWrapper
+					justify={'space-between'}
+					align={'center'}
+				>
+					<Logo />
+					<Menu />
+				</FlexWrapper>
+			</Container>
 		</StyledHeader>
-	);
-};
+	)
+}
 
-const StyledHeader = styled.header`
-	display: flex;
-	justify-content: space-between;
-`;
+const StyledHeader = styled.header``
---
+++ src/styles/Theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+	colors: {
+		primaryBg: '#fafafa',
+		secondaryBg: '#f5f2fd',
+		accent: '#5222d0',
+
+		fontText: '#1f2626',
+		fontTitle: '#151717',
+	},
+}
